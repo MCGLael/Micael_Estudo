eb8c0c8dd30febb10dc101446cd86893f45d1526
MCGLael <micaelllcavalcante@gmail.com>
2026-05-22 18:22:04 -0300

    Criação do conversor de temperatura Celsius Fahrenheit

---
+++ Portifolio/Iniciante/JogoAdivinha.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portifolio.Iniciante
+{
+
+    internal class JogoAdivinha
+    {
+        public static void Executar()
+        {
+
+            Console.WriteLine("Bem vindo ao jogo de adivinha, você terá 5 chances para adivinhar um numero entre 0 e 10 ");
+            int opcao;
+            int i = 0;
+            //int random;
+            int random = new Random().Next(0, 11);
+            do
+            {
+                i++;
+                Console.WriteLine($"{i}/5 Chances");
+                
+                Console.WriteLine("\nDigite um numero entre 0 a 10");
+
+                if (!int.TryParse(Console.ReadLine(), out opcao)|| opcao > 10 || opcao <0)
+                {Console.WriteLine("Opção invalida, tente novamente");continue; }
+
+                
+                if (opcao == random)
+                {
+                    Console.WriteLine("\nCorreto! Meus parabens, você ganhou!");
+                    
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("\nErrado!");
+                    
+                }
+                
+
+            } while ( i < 5);
+            Console.WriteLine("\nSuas chances acabaram, o numero correto era: {0} ToT", random);
+
+        }
+    }
+
+
+}
+
+
--- Portifolio/Program.cs
+++ Portifolio/Program.cs
@@ -11,7 +11,7 @@ namespace Portifolio.Iniciante
 
         public static void Main()
         {
-            Console.WriteLine("Escolha o exemplo que quer executar: \n1 -> Calculadora\n2 -> Conversor de temperatura");
+            Console.WriteLine("Escolha o exemplo que quer executar: \n1 -> Calculadora\n2 -> Conversor de temperatura\n3 -> Jogo de Advinha");
             if (!int.TryParse(Console.ReadLine(), out int opcao))
             {
                 Console.WriteLine("Opção invalida. Encerrando programa...");
@@ -25,6 +25,9 @@ namespace Portifolio.Iniciante
                     break;
                 case 2:
                     ConversorTemperatura.Executar();
+                    break;
+                case 3:
+                    JogoAdivinha.Executar();
                     break;
                 default:
                     Console.WriteLine("Opção invalida, encerrando programa...");
