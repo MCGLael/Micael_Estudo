894a3da2e16209cb3ec1d19d95b9f3162ca62ad9
MCGLael <micaelllcavalcante@gmail.com>
2026-06-02 19:19:58 -0300

    Rock Paper Scissor complete, adding validation, and the game's logic

--- Portifolio/Iniciante/PedraPapelTesoura.cs
+++ Portifolio/Iniciante/PedraPapelTesoura.cs
@@ -21,19 +21,33 @@ namespace Portifolio.Iniciante
             Console.WriteLine("Welcome to Rock Paper Scissors game!!!");
             
             var update = false;
-            while(!update == true)
+            while (!update == true)
             {
-                Console.WriteLine("Rock -> 1\nPaper -> 2\nScissors -> 3\nExit -> 0");
-                if (!int.TryParse(Console.ReadLine(), out int option)) { Console.WriteLine("Invalid"); }
+                Console.WriteLine("Rock -> 1\nPaper -> 2\nScissors -> 3\nExit -> 0\n");
+
+                if (!int.TryParse(Console.ReadLine(), out int option) || option < 0 || option > 3) { Console.WriteLine("Invalid\n"); continue; }
+                if (option == 0) { update = true; continue; }
+
                 int jogada = new Random().Next(1, 4);
                 Options pc_opt = (Options)jogada;
-                if(option == (int)pc_opt)
+                Options player = (Options)option;
+                if (option == (int)pc_opt)
                 {
-                    Console.WriteLine("Draw");
-                }else if(option == 1 && (int)pc_opt == 2)
                     Console.WriteLine($"Computer: {pc_opt}");
-                    Console.WriteLine($"You: {option}");
-                Console.WriteLine("You Lose!");
+                    Console.WriteLine($"You: {player}");
+                    Console.WriteLine("Draw!!!\n");
+                } else if (option == 1 && (int)pc_opt == 3 || option == 2 && (int)pc_opt == 1 || option == 3 && (int)pc_opt == 2) { 
+                    Console.WriteLine($"Computer: {pc_opt}");
+                Console.WriteLine($"You: {player}");
+                Console.WriteLine("You Win!!!\n"); }
+                else
+                {
+                    Console.WriteLine($"Computer: {pc_opt}");
+                    Console.WriteLine($"You: {player}");
+                    Console.WriteLine("You Lose!!!\n");
+                }
+                
+
 
             }
             Console.WriteLine("Thank you for playing");
