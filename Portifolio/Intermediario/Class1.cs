6bc38a7b2f8b00156beb8c7fe9cce129b6afe481
MCGLael <micaelllcavalcante@gmail.com>
2026-06-08 18:22:20 -0300

    Criação da pasta de exercicios nivel Intermediario Criação do Exercicio de RPG Criação de Classe Base "Role" e molde das Classes que terão no Exercicio

---
+++ Portifolio/Intermediario/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portifolio.Intermediario
+{
+    public class Role
+    {
+        public string Name { get; set; }
+        public int Hp { get; set; }
+
+        public int Damage { get; set; }
+
+        public int Defense { get; set; }
+
+        public int Attack()
+        {
+
+            return Damage;
+        }
+
+        public int HealingPotion()
+        {
+            return Hp + 20;
+
+        }
+    }
+
+    public class Knight : Role
+    {
+
+    }
+    public class Mage : Role
+    {
+
+    }
+    public class Tank : Role
+    {
+
+    }
+    public class Heroe : Role
+    {
+
+    }
+
+
+    internal class Class1
+    {
+    }
+}
